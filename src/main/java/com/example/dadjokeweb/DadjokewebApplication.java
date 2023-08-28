10764cf5f5bcbf0f37081da94a91bd999085ce04
JoJaWiley <joshua.wiley@summitoic.edu>
2023-08-28 18:12:18 -0500

    Add existing project files to Git

---
+++ src/main/java/com/example/dadjokeweb/DadjokewebApplication.java
@@ -0,0 +1,13 @@
+package com.example.dadjokeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadjokewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadjokewebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dadjokeweb/DadjokewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dadjokeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadjokewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
